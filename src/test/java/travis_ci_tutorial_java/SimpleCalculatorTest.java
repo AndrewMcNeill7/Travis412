7a9da176597af96de1e0654eb23ad0467a43b9e8
AndrewMcNeill7 <100299220+AndrewMcNeill7@users.noreply.github.com>
2025-03-08 12:21:25 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -7,36 +7,7 @@ public class SimpleCalculatorTest {
     @Test
     public void testAdd() {
         SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(calc.add(1, 1), 2);
+        assertEquals(calc.add(1, 1), 0);
     }
 
-    @Test
-    public void testSubtract() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(calc.subtract(5, 3), 2);
-    }
-
-    @Test
-    public void testSubtractAgain() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(calc.subtract(5, 3), 2);  // Additional test for subtraction
-    }
-
-    @Test
-    public void testMultiply() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(calc.multiply(4, 3), 12);
-    }
-
-    @Test
-    public void testDivide() {
-        SimpleCalculator calc = new SimpleCalculator();
-        assertEquals(calc.divide(10, 2), 5);
-    }
-
-    @Test(expected = ArithmeticException.class)
-    public void testDivideByZero() {
-        SimpleCalculator calc = new SimpleCalculator();
-        calc.divide(5, 0);
-    }
 }
